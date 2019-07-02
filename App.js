7ed7c33eaa4e5f6f6811a80456cc70c165e6d7dc
BrowerDevelopments <jcb8561@rit.edu>
2019-07-02 13:10:17 -0400

    Tree Design

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, PanResponder, Animated, Text, View } from 'react-native';
+import Leaf from '/Users/jacobbrower/Documents/ReactNative/MindMapping/assets/Components/Leaf.js';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Leaf buttonStyles={styles.button}
+        textStyles={styles.text}
+        content={'This is content'}
+        />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#f3f3',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 14,
   },
 });
---
+++ assets/Components/Leaf.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+import {Text, TouchableOpacity} from 'react-native';
+
+class Leaf extends Component {
+     static propTypes = {
+          content: PropTypes.string.isRequired,
+          textStyles: PropTypes.oneOfType( [PropTypes.array, PropTypes.number, PropTypes.shape({})]).isRequired,
+          buttonStyles: PropTypes.oneOfType([PropTypes.array, PropTypes.number, PropTypes.shape({})]).isRequired,
+     }
+
+     render = () => {
+          const {textStyles, buttonStyles, content} = this.props;
+
+          return (
+               <TouchableOpacity style={buttonStyles}>
+                    <Text style={textStyles}>{content}</Text>
+               </TouchableOpacity>
+          )
+     }
+}
+
+export default Leaf;
